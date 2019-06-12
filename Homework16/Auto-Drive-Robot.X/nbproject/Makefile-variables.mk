8a0798bea41659b431eb3b8493916cb25040ede5
Hang Yuan <JohannYuan@gmail.com>
2019-06-12 14:05:22 -0500

    In progress of final robot.

---
+++ Homework16/Auto-Drive-Robot.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Auto-Drive-Robot.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Auto-Drive-Robot.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=auto-drive-robot.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/auto-drive-robot.x.tar
